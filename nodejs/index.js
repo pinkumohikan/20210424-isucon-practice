c5b22f4024220a9266ad678dc3013e9f15d9310b
Hokuto Shinoda <pinkumohikan+github@gmail.com>
2021-04-24 17:51:59 +0900

    Revert "GET /historyでのN+1を軽減"

--- nodejs/index.js
+++ nodejs/index.js
@@ -323,62 +323,44 @@ function getHistory(req, res) {
   const { channelId } = req.params
   let page = parseInt(req.query.page || '1')
 
-  if (isNaN(page) || page < 1) {
-    res.status(400).end()
-    return
-  }
-
   const N = 20
   return pool.query('SELECT COUNT(*) as cnt FROM message WHERE channel_id = ?', [channelId])
     .then(([row2]) => {
       const cnt = row2.cnt
       const maxPage = Math.max(Math.ceil(cnt / N), 1)
-      if (page > maxPage) {
+
+      if (isNaN(page) || page < 1 || page > maxPage) {
         res.status(400).end()
         return
       }
 
-      return pool.query(`
-        SELECT
-            m.id,
-            m.created_at,
-            m.content,
-            u.name as user_name,
-            u.display_name as user_display_name,
-            u.avatar_icon as user_avatar_icon
-        FROM
-            message as m
-            inner join user as u on u.id = m.user_id
-        WHERE
-            m.channel_id = ?
-        ORDER BY m.id DESC
-        LIMIT ?
-        OFFSET ?`, [channelId, N, (page - 1) * N])
+      return pool.query('SELECT * FROM message WHERE channel_id = ? ORDER BY id DESC LIMIT ? OFFSET ?', [channelId, N, (page - 1) * N])
         .then(rows => {
           const messages = []
-
+          let p = Promise.resolve()
           rows.forEach(row => {
-            messages.push({
-              id: row.id,
-              user: {
-                name: row.user_name,
-                display_name: row.user_display_name,
-                avatar_icon: row.user_avatar_icon,
-              },
-              date: formatDate(row.created_at),
-              content: row.content,
+            const r = {}
+            r.id = row.id
+            p = p.then(() => {
+              return pool.query('SELECT name, display_name, avatar_icon FROM user WHERE id = ?', [row.user_id])
+                .then(([user]) => {
+                  r.user = user
+                  r.date = formatDate(row.created_at)
+                  r.content = row.content
+                  messages.push(r)
+                })
             })
           })
 
-          messages.reverse()
-
-          // FIXME: ここでもchannel idからchannelを求めるクエリ投げてる
-          return getChannelListInfo(pool, channelId)
-            .then(({ channels, description }) => {
-              res.render('history', {
-                req, channels, channelId, messages, maxPage, page,
+          return p.then(() => {
+            messages.reverse()
+            return getChannelListInfo(pool, channelId)
+              .then(({ channels, description }) => {
+                res.render('history', {
+                  req, channels, channelId, messages, maxPage, page,
+                })
               })
-            })
+          })
       })
     })
 }
